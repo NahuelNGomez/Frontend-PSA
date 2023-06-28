20040408ac1bd0eef220abda415a6a92d4d6120d
MiguelV5 <mvasquezj@fi.uba.ar>
2023-06-28 03:20:27 -0300

    comment out old reportemodal + extract reporte form

--- pages/recursos/[id]/reporte.tsx
+++ pages/recursos/[id]/reporte.tsx
@@ -2,6 +2,7 @@ import { ReactElement, useEffect, useState } from "react"
 import RegistrosTable from "../../../components/Recursos/RegistrosTable";
 import { useRouter } from "next/router";
 import Breadcrumbs from "../../../components/Breadcrumbs";
+import ReporteHorasForm from "../../../components/Recursos/ReporteHorasForm";
 
 function BreadcrumbItemsReporte({ legajo_recurso }: any) {
     return [
@@ -21,55 +22,6 @@ function BreadcrumbItemsReporte({ legajo_recurso }: any) {
     ]
 }
 
-function ReporteHorasForm(
-    {
-        handleSubmit,
-        project,
-        projects,
-        setProject,
-        dateInicio,
-        setDateInicio,
-        dateFin,
-        setDateFin,
-        maxDate,
-        anyEmpty
-    }: any
-) {
-    return (
-        <form onSubmit={handleSubmit}>
-            <h1 className="modal-title fs-5">Hacer reporte</h1>
-            <div className="mb-3">
-                <label htmlFor="name" className="col-form-label">
-                    Proyecto: <small>(requerido)</small>
-                </label>
-                <select className="form-select" value={project} onChange={(e) => setProject(e.target.value)}>
-                    {projects.map((proyect: any) => (
-                        <option key={proyect["id"]} value={proyect["id"]}>
-                            {proyect["name"]}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="start_date" className="col-form-label">
-                    Fecha de inicio: <small>(requerido)</small>
-                </label>
-                <input type="date" className="form-control" id="date" required value={dateInicio} max={maxDate} onChange={(e) => setDateInicio(e.target.value)} />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="start_date" className="col-form-label">
-                    Fecha de finalizacion: <small>(requerido)</small>
-                </label>
-                <input type="date" className="form-control" id="date" required value={dateFin} max={maxDate} min={dateInicio} onChange={(e) => setDateFin(e.target.value)} />
-            </div>
-            <div className="text-center">
-                <button type="submit" disabled={anyEmpty} className="btn btn-primary">
-                    Generar
-                </button>
-            </div>
-        </form>
-    );
-}
 
 function LoadingIndicator({ cargando }: any) {
     return <div>{cargando}</div>;
